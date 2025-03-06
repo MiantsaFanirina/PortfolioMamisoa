ac5e1a8460f13860e2760be2bc081e436631fad3
MiantsaFanirina <miantsafanirinarakotondrafara@gmail.com>
2025-03-07 00:46:03 +0400

    fix dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={'!scroll-smooth'}>
+    <html lang="en" className={'!scroll-smooth dark'} data-theme={'dark'}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
