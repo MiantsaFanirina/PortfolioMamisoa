cf0f14a39253ef76b564c97469fd81c0da3a4b5f
MiantsaFanirina <miantsafanirinarakotondrafara@gmail.com>
2025-03-07 15:28:59 +0400

    Update Data

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Image from "next/image";
+import MagicButton from "./ui/magic-button";
+import {FaLocationArrow} from "react-icons/fa6";
 
 const About = () => {
     return (
@@ -21,12 +23,32 @@ const About = () => {
                 `}
             />
 
-            <h3 className={'font-electro tracking-widest font-semibold text-xl md:text-2xl mt-10'}>I&apos;m <span className={'text-purple'}>MAMISOA HYACINTHE</span></h3>
+            <h3 className={'font-electro tracking-widest font-semibold text-xl md:text-2xl mt-10'}>I&apos;m <span
+                className={'text-purple'}>MAMISOA HYACINTHE</span></h3>
 
             <p className={'text-center text-md md:text-lg md:px-10 lg:px-36 mt-3'}>
-                As a business application developer (WEB, Mobile, Desktop) with <span className="font-bold">10 years of experience</span> in <span className="font-bold">.NET C#</span>, <span className="font-bold">PHP</span>, <span className="font-bold">Java</span>, <span className="font-bold">JavaScript</span>, and <span className="font-bold">TypeScript</span>, I stand out for my <span className="font-bold">leadership</span> and <span className="font-bold">strong communication skills</span>. My <span className="font-bold">extensive experience</span> has also taught me to continuously <span className="font-bold">adapt</span> in the ever-evolving field of <span className="font-bold">information technology</span>.
-                I am eager to join your company to share my <span className="font-bold">expertise</span> and <span className="font-bold">passion</span> for <span className="font-bold">IT</span> while contributing to your projects&apos; <span className="font-bold">success</span>. I am <span className="font-bold">available immediately</span> and open to an <span className="font-bold">interview</span> at your convenience. Additionally, I hold a <span className="font-bold">valid passport</span> for any potential <span className="font-bold">international opportunities</span>.
+                As a business application developer (WEB, Mobile, Desktop) with <span className="font-bold">10 years of experience</span> in <span
+                className="font-bold">.NET C#</span>, <span className="font-bold">PHP</span>, <span
+                className="font-bold">Java</span>, <span className="font-bold">JavaScript</span>, and <span
+                className="font-bold">TypeScript</span>, I stand out for my <span
+                className="font-bold">leadership</span> and <span
+                className="font-bold">strong communication skills</span>. My <span className="font-bold">extensive experience</span> has
+                also taught me to continuously <span className="font-bold">adapt</span> in the ever-evolving field
+                of <span className="font-bold">information technology</span>.
+                I am eager to join your company to share my <span className="font-bold">expertise</span> and <span
+                className="font-bold">passion</span> for <span className="font-bold">IT</span> while contributing to
+                your project&apos;s <span className="font-bold">success</span>. I am <span className="font-bold">available immediately</span> and
+                open to an <span className="font-bold">interview</span> at your convenience.
             </p>
+
+
+            <a download={true} href="/cv-mamisoa.pdf" className={'mt-10'}>
+                <MagicButton
+                    title={'Get my Resume'}
+                    icon={<FaLocationArrow/>}
+                    position={'right'}
+                />
+            </a>
 
         </section>
     );
--- data/index.tsx
+++ data/index.tsx
@@ -7,7 +7,6 @@ import {
     LightBulbIcon,
     UsersIcon
 } from "@heroicons/react/outline";
-import Image from "next/image";
 import React from "react";
 import {FaRegCircle} from "react-icons/fa6";
 
@@ -62,7 +61,6 @@ export const FeaturesData = [
     },
 ];
 
-
 export const EducationData = [
     {
         id: 1,
@@ -79,7 +77,7 @@ export const EducationData = [
     {
         id: 3,
         title: "2012 - Master's Degree in Engineering Informatics",
-        desc: "Geneva School of Engineering [E.I.G]",
+        desc: "Ecole d'Ingénieur de Genève [E.I.G]",
         className: "md:col-span-2", // change to md:col-span-2
     },
 ];
@@ -383,8 +381,73 @@ export const WorkData = [
         ),
         className: 'md:col-span-2'
     },
+
     {
         id: 6,
+        thumbnail: ['Dec 2015', 'Jan 2015'],
+        title: 'Business Application Developer for Employees Management',
+        location: 'QMM Consultant Madagascar',
+        github: "https://github.com/Andriamiamina/QMM",
+        stacks: [
+            'ASP.NET MVC', 'Entity Framework', 'MSSQL', 'SignalR', 'Hangfire','AngularJS'
+        ],
+        content: (
+            <div>
+                {/*List*/}
+
+                <h2 className={'text-[25px] md:text-lg lg:text-xl text-white relative mb-3 font-bold'}>
+                    Main Responsibilities:
+                </h2>
+
+                <div className={'flex flex-col gap-2 mb-10'}>
+                    <div
+                        className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-md md:text-lg">
+                        <FaRegCircle className={'text-purple shrink-0'}/>
+                        <span>
+                            Manage the authentication and authorization of each user (Administrator, Data Entry, HR, Finance)
+                        </span>
+                    </div>
+
+                    <div
+                        className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-md md:text-lg">
+                        <FaRegCircle className={'text-purple shrink-0'}/>
+                        <span>
+                            Creation and submission of a request for cash advance, letter of coverage, expense report, and medical expense reimbursement request.
+                            In addition, validation of a request.
+                        </span>
+                    </div>
+
+                    <div
+                        className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-md md:text-lg">
+                        <FaRegCircle className={'text-purple shrink-0'}/>
+                        <span>
+                            Display of parameters on standby allowances, temporary work, relocation allowance, period, team, model.
+                            Integration of data for 500 employees from an Excel file.
+                            Modification of timesheets and automatic email sending to team leaders in case of rejection.
+                        </span>
+                    </div>
+                    <div
+                        className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-md md:text-lg">
+                        <FaRegCircle className={'text-purple shrink-0'}/>
+                        <span>
+                            Management of public holidays.Management of period, model, team (Add, modify, delete).
+                            Calculation of data to be provided to SAGE.
+                        </span>
+                    </div>
+                    <div
+                        className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-md md:text-lg">
+                        <FaRegCircle className={'text-purple shrink-0'}/>
+                        <span>
+                            Creation and writing of Software Specifications documents.
+                        </span>
+                    </div>
+                </div>
+            </div>
+        ),
+        className: 'col-span-1'
+    },
+    {
+        id: 7,
         thumbnail: ['Dec 2014', 'Jan 2014'],
         title: 'Business Application Developer for Money Transfer',
         location: 'Cash-Xpress Genève & Pacoservices Lausanne',
@@ -446,13 +509,12 @@ export const WorkData = [
     },
 ];
 
-
 export const navItems = [
     {name: 'Home', link: '/'},
-    { name: "About", link: "#about" },
-    { name: "Offers", link: "#offers" },
-    { name: "Experiences", link: "#experiences" },
-    { name: "Contact", link: "#contact" },
+    {name: "About", link: "#about"},
+    {name: "Offers", link: "#offers"},
+    {name: "Experiences", link: "#experiences"},
+    {name: "Contact", link: "#contact"},
 ];
 
 export const gridItems = [
@@ -516,138 +578,6 @@ export const gridItems = [
         titleClassName: "justify-center md:max-w-full max-w-60 text-center",
         img: "",
         spareImg: "",
-    },
-];
-
-export const projects = [
-    {
-        id: 1,
-        title: "3D Solar System Planets to Explore",
-        des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-        img: "/p1.svg",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-        link: "https://github.com/adrianhajdin?tab=repositories",
-    },
-    {
-        id: 2,
-        title: "Yoom - Video Conferencing App",
-        des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-        img: "/p2.svg",
-        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-        link: "https://github.com/adrianhajdin/zoom-clone",
-    },
-    {
-        id: 3,
-        title: "AI Image SaaS - Canva Application",
-        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/p3.svg",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-        link: "https://github.com/adrianhajdin/ai_saas_app",
-    },
-    {
-        id: 4,
-        title: "Animated Apple Iphone 3D Website",
-        des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-        img: "/p4.svg",
-        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-        link: "https://github.com/adrianhajdin/iphone",
-    },
-];
-
-export const testimonials = [
-    {
-        quote:
-            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-        name: "Michael Johnson",
-        title: "Director of AlphaStream Technologies",
-    },
-    {
-        quote:
-            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-        name: "Michael Johnson",
-        title: "Director of AlphaStream Technologies",
-    },
-    {
-        quote:
-            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-        name: "Michael Johnson",
-        title: "Director of AlphaStream Technologies",
-    },
-    {
-        quote:
-            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-        name: "Michael Johnson",
-        title: "Director of AlphaStream Technologies",
-    },
-    {
-        quote:
-            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-        name: "Michael Johnson",
-        title: "Director of AlphaStream Technologies",
-    },
-];
-
-export const companies = [
-    {
-        id: 1,
-        name: "cloudinary",
-        img: "/cloud.svg",
-        nameImg: "/cloudName.svg",
-    },
-    {
-        id: 2,
-        name: "appwrite",
-        img: "/app.svg",
-        nameImg: "/appName.svg",
-    },
-    {
-        id: 3,
-        name: "HOSTINGER",
-        img: "/host.svg",
-        nameImg: "/hostName.svg",
-    },
-    {
-        id: 4,
-        name: "stream",
-        img: "/s.svg",
-        nameImg: "/streamName.svg",
-    },
-    {
-        id: 5,
-        name: "docker.",
-        img: "/dock.svg",
-        nameImg: "/dockerName.svg",
-    },
-];
-
-export const workExperience = [
-    {
-        id: 1,
-        title: "Frontend Engineer Intern",
-        desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-        className: "md:col-span-2",
-        thumbnail: "/exp1.svg",
-    },
-    {
-        id: 2,
-        title: "Mobile App Dev - JSM Tech",
-        desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-        className: "md:col-span-2", // change to md:col-span-2
-        thumbnail: "/exp2.svg",
-    },
-    {
-        id: 3,
-        title: "Freelance App Dev Project",
-        desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-        className: "md:col-span-2", // change to md:col-span-2
-        thumbnail: "/exp3.svg",
-    },
-    {
-        id: 4,
-        title: "Lead Frontend Developer",
-        desc: "Developed and maintained user-facing features using modern frontend technologies.",
-        className: "md:col-span-2",
-        thumbnail: "/exp4.svg",
     },
 ];
 
